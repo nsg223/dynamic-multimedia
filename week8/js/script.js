db11e15aeb13219188999faf2c1a5f1c76226883
Nate Sanchez-Goodwin <Nate@nate-mac.BigPond>
2016-08-08 18:52:24 +1000

    do week 8 + 9

--- week8/js/script.js
+++ week8/js/script.js
@@ -2,44 +2,75 @@ $(document).ready(function() {
 
     items = [];
 
-    $.getJSON('data/items.json', function(data) {
-        items = data;
+    function get_items(request_url, callback) {
+
+    $.getJSON(request_url, function(data) {
+        console.log(data);
+        data = data.photos.photo;
+        console.log(data);
+        items = [];
+
+        for (var i = 0; i < data.length; i++) {
+            url = 'https://farm' + data[i].farm + '.staticflickr.com/' + data[i].server +'/' + data[i].id + '_'+ data[i].secret +'_b.jpg';
+            thumb = 'https://farm' + data[i].farm + '.staticflickr.com/' + data[i].server +'/' + data[i].id + '_'+ data[i].secret +'_m.jpg';
+            temp = {
+                "image": url,
+                "thumb": thumb,
+                "caption": data[i].title
+            }
+            log_size(data[i].id);
+            items.push(temp);
+        }
+        if (callback) {
+            callback();
+        }
     });
+
+    }
+
+    function log_size(photo_id) {
+
+        request_url = 'https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=bd34833c1dc13ee8d364413c483f18dc&photo_id='+ photo_id +'&format=json&nojsoncallback=1';
+        $.getJSON(request_url, function(data) {
+            console.log(data);
+        });
+
+    }
+
+    get_items('https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=2564c5860d587b11245b33f7e4b25d6a&per_page=20&format=json&nojsoncallback=1');
 
     $("#form-submit").submit(function (e){
         e.preventDefault();
         var query = $("#search-form").val();
         query = query.toLowerCase();
         query = query.trim();
-        results = [];
 
-        for (i = 0; i < items.length; i++) {
-            caption = items[i].caption;
-            caption = caption.toLowerCase();
-            if (caption.indexOf(query) != -1) {
-                results.push(items[i]);
-            }    
-        }
-
-        if (results.length > 0) {
-            render(results);
-        } else if (query == '') {
-            render(items);
-        } else {
-            $("#content-section").text('No Results Found');
-        }
+        olditems = items;
+        
+        get_items('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a4dc801fcd306e0c7494ffee49b598ce&text=' + query + '&per_page=20&format=json&nojsoncallback=1', function() {
+            if (items.length > 0) {
+                render(items);
+            } else if (query == '') {
+                render(olditems);
+            } else {
+                $("#content-section").text('No Results Found');
+            }
+        });
 
     });
 
     function render(render_items) {
 
-        content = '<div class="row"> <button id="back" class="btn btn-primary">Back...</button> </div>';
+        content = '<div class="row"> <button id="back" class="btn btn-primary">Back...</button> <p>&nbsp; </p> </div><div class="row">';
 
         for (var i = 0; i < render_items.length; i++) {
+            if (i % 3 == 0 || i == 0) { 
+                content +='</div><div class="row">'
+            }
             content += '<div class="col-md-4">';
             content += '<div class="well">';
             content += '<a href="' + render_items[i].image + '" data-lightbox="images" data-title="' + render_items[i].caption + '">';
-            content += '<img src="' + render_items[i].image + '" alt="lovely photo" class="center-block img-responsive">';
+            content += '<img src="' + render_items[i].thumb + '" alt="lovely photo" class="center-block img-responsive">';
             content += '</a>';
             content +=  '<p class="text-center"> ' + render_items[i].caption + ' </p>';
             content += '</div>';
