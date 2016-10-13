c41065b753e3bf8b93c7ed54464702ba0bc7ff3f
Nate Sanchez-Goodwin <nsg223@gmail.com>
2016-10-13 23:22:52 +1000

    fblogin controller log response

--- assignment/js/controller.js
+++ assignment/js/controller.js
@@ -5,6 +5,7 @@ $(document).on('fblogin', function() {
     FB.api(
         "/815157038515764",
         function (response) {
+        console.log(response);
         if (response && !response.error) {
             /* handle the result */
             console.log(response);
