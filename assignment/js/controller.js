dd55ae95817638b7beabf869831d6ecc12607cd8
Nate Sanchez-Goodwin <nsg223@gmail.com>
2016-10-13 23:16:18 +1000

    fb page api attempt

--- assignment/js/controller.js
+++ assignment/js/controller.js
@@ -1,5 +1,14 @@
 app.controller('contentCtrl', function($scope, $http) {
     
+FB.api(
+    "/815157038515764",
+    function (response) {
+      if (response && !response.error) {
+        /* handle the result */
+        console.log(response);
+      }
+    }
+);
 
 
 });
