9cefcf5caab30482542844f681e6a0cd10610e64
Nate Sanchez-Goodwin <nsg223@gmail.com>
2016-10-15 18:43:18 +1000

    try stufffs

--- assignment/js/controller.js
+++ assignment/js/controller.js
@@ -21,17 +21,17 @@ app.controller('contentCtrl', function($scope, $http) {
 
                     for (var i = 0; i < $scope.albums.length; i++) {
                         console.log($scope.albums[i].location);
-                        // $http.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+ $scope.albums[i].location +'%2C%20au%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
-                        //     .then(function(response){
+                        $http.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+ $scope.albums[i].location +'%2C%20au%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
+                            .then(function(response){
 
-                        //         console.log(response.data);
+                                console.log(response.data);
 
-                        //         // $scope.$apply(function() {
-                        //             $scope.albums[i].weather = response.data.query.results.channel;
-                        //         //     console.log(response.data);
-                        //         // });
+                                // $scope.$apply(function() {
+                                    $scope.albums[i].weather = response.data.query.results.channel;
+                                //     console.log(response.data);
+                                // });
                                 
-                        //     });
+                            });
                     }
             });
             
