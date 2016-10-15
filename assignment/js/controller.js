941dd0b31b1af52ac3a6908f6b7284dcf7a433b0
Nate Sanchez-Goodwin <nsg223@gmail.com>
2016-10-15 19:23:59 +1000

    debug albumdata

--- assignment/js/controller.js
+++ assignment/js/controller.js
@@ -64,6 +64,7 @@ app.controller('contentCtrl', function($scope, $http) {
 
                 getAlbumInfo($routeParams.id, function(albumData) {
                 $scope.album = albumData;
+                console.log(albumData);
                 $http.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+ albumData.location +'%2C%20au%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
                     .then(function(response){
                         $scope.album.weather = response.data.query.results.channel;
