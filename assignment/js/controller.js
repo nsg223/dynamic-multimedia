dc9a81ddd0db1907dfe5c478b28944c047a880f4
Nate Sanchez-Goodwin <nsg223@gmail.com>
2016-10-15 18:24:34 +1000

    try get weather for locations

--- assignment/js/controller.js
+++ assignment/js/controller.js
@@ -15,9 +15,19 @@ app.controller('contentCtrl', function($scope) {
         });
     
         getAlbums(function(data) {
+
             $scope.$apply(function() {
                 $scope.albums = data;
             });
+            
+            for (var i = 0; i < data.length; i++) {
+                $http.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+ $scope.albums[i].location +'%2C%20au%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
+                    .then(function(response){
+                        // $scope.weather = response.data.query.results.channel;
+                        console.log(response.data);
+                    });
+            }
+            
         });
 
         getFeed(function(data) {
@@ -32,7 +42,7 @@ app.controller('contentCtrl', function($scope) {
 /**
  * Controller for  Album Individual view
  */
-.controller('albumCtrl', function($scope, $routeParams) {
+.controller('albumCtrl', function($scope, $routeParams, $https) {
     
     if (userId == undefined)
         window.location = "#/";
