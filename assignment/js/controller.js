a246aaba01fa81e9bcd8815283720d9860b7b0f4
Nate Sanchez-Goodwin <nsg223@gmail.com>
2016-10-15 18:33:48 +1000

    fix scope not updating

--- assignment/js/controller.js
+++ assignment/js/controller.js
@@ -23,8 +23,12 @@ app.controller('contentCtrl', function($scope, $http) {
             for (var i = 0; i < data.length; i++) {
                 $http.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+ data.albums[i].location +'%2C%20au%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
                     .then(function(response){
-                        $scope.albums[i].weather = response.data.query.results.channel;
-                        console.log(response.data);
+
+                        $scope.$apply(function() {
+                            $scope.albums[i].weather = response.data.query.results.channel;
+                            console.log(response.data);
+                        });
+                        
                     });
             }
             
